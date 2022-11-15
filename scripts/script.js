1f8e68d13fd4dd999590bc88087c81b344f54300
koniro <koniro1403@gmail.com>
2022-11-15 13:44:45 +0600

    empty

---
+++ scripts/script.js
@@ -0,0 +1,102 @@
+const signBtn = document.querySelector(".sign__btn")
+console.log(signBtn);
+// orderBtn.onclick = function() {
+//     window.location.href = 'https://example.com';
+// }
+
+signBtn.addEventListener("click", function(event){
+    window.location.href = 'https://example.com';
+    // alert("Pressed");
+})
+
+const orderBtn = document.querySelector(".order__btn")
+console.log(orderBtn);
+// orderBtn.onclick = function() {
+//     window.location.href = 'https://example.com';
+// }
+
+orderBtn.addEventListener("click", function(event){
+    window.location.href = 'https://example.com';
+    // alert("Pressed");
+})
+
+// reason__card--btn
+const reasonBtn1 = document.querySelector("#rc1")
+console.log(reasonBtn1);
+// orderBtn.onclick = function() {
+//     window.location.href = 'https://example.com';
+// }
+
+reasonBtn1.addEventListener("click", function(event){
+    window.location.href = 'https://example.com';
+    // alert("Pressed");
+})
+
+const reasonBtn2 = document.querySelector("#rc2")
+console.log(reasonBtn2);
+// orderBtn.onclick = function() {
+//     window.location.href = 'https://example.com';
+// }
+
+reasonBtn2.addEventListener("click", function(event){
+    window.location.href = 'https://example.com';
+    // alert("Pressed");
+})
+
+const reasonBtn3 = document.querySelector("#rc3")
+console.log(reasonBtn3);
+// orderBtn.onclick = function() {
+//     window.location.href = 'https://example.com';
+// }
+
+reasonBtn3.addEventListener("click", function(event){
+    window.location.href = 'https://example.com';
+    // alert("Pressed");
+})
+
+const formBtn = document.querySelector(".sub__form--btn")
+console.log(formBtn);
+// orderBtn.onclick = function() {
+//     window.location.href = 'https://example.com';
+// }
+
+formBtn.addEventListener("click", function(event){
+    // window.location.href = 'https://example.com';
+    alert("Pressed");
+})
+
+//media icons
+const instBtn = document.querySelector("#inst")
+console.log(instBtn);
+// orderBtn.onclick = function() {
+//     window.location.href = 'https://example.com';
+// }
+
+instBtn.addEventListener("click", function(event){
+    window.location.href = 'https://example.com';
+    // alert("Pressed");
+})
+
+const linkedinBtn = document.querySelector("#linkedin")
+console.log(linkedinBtn);
+// orderBtn.onclick = function() {
+//     window.location.href = 'https://example.com';
+// }
+
+linkedinBtn.addEventListener("click", function(event){
+    window.location.href = 'https://example.com';
+    // alert("Pressed");
+})
+
+const twitterBtn = document.querySelector("#twitter")
+console.log(twitterBtn);
+// orderBtn.onclick = function() {
+//     window.location.href = 'https://example.com';
+// }
+
+twitterBtn.addEventListener("click", function(event){
+    window.location.href = 'https://example.com';
+    // alert("Pressed");
+})
+
+//carousel
